2cb9edcd0982199047a24ed5fb6f44b6a1e555ab
bingyue zhang <15727335097@163.com>
2020-04-26 16:57:29 +0800

    commit

---
+++ sql/src/main/java/com/example/sql/config/DataSourceConfig.java
@@ -0,0 +1,28 @@
+package com.example.sql.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean(name = "primaryDataSource")
+    @Qualifier("primaryDataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.primary")
+    public javax.sql.DataSource primaryDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "secondaryDataSource")
+    @Qualifier("secondaryDataSource")
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource.secondary")
+    public javax.sql.DataSource secondaryDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+}
---
+++ sql/src/test/java/com/example/sql/SqlApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.sql;
+
+import com.example.sql.config.DataSourceConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+
+@SpringBootTest
+class SqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
